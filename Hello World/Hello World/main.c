0f26430de5c23d677c8bcafc02afb5dd809b4c0c
DNIIBOY <daniel@nettelfield.com>
2022-10-11 09:36:42 +0200

    ehhh weird random flash thing

--- Hello World/Hello World/main.c
+++ Hello World/Hello World/main.c
@@ -7,6 +7,7 @@
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/power.h>
 #include <util/delay.h>
 
 #define FORSINKELSE 85
@@ -14,21 +15,44 @@
 
 int main(void)
 {
-	uint8_t i=0;
-	DDRD |= 0xFF;
+  uint8_t i;
+  uint8_t gentag;
+  uint8_t hvilkenLED;
+  uint8_t randomNumber = 0x1234;
 
-    while (1){
-		while (i<7){
-			PORTD = (1 << i);
-			_delay_ms(FORSINKELSE);
-			i++;
-		}
-		while (i>0){
-			PORTD = (1<<i);
-			_delay_ms(FORSINKELSE);
-			i--;
-		}
+  DDRD |= 0xFF;
+
+  while (1){
+    for (i = 0; i < 8; i++){
+      PORTD |= (1 << i);
+      _delay_ms(FORSINKELSE);
     }
-	return 0;
+
+    for (i = 0; i < 8; i++){
+      PORTD &= ~(1 << i);
+      _delay_ms(FORSINKELSE);
+    }
+    _delay_ms(5 * FORSINKELSE);
+
+    for (i = 7; i < 255; i--){
+      PORTD |= (1 << i);
+      _delay_ms(FORSINKELSE);
+    }
+    for (i = 7; i < 255; i--){
+      PORTD &= (1 << i);
+      _delay_ms(FORSINKELSE);
+    }
+    _delay_ms(5 * FORSINKELSE);
+
+    for (gentag = 0; gentag < 75; gentag++){
+      randomNumber = 2053 * randomNumber + 13849;
+      hvilkenLED = (randomNumber >> 8) & 0b00000111;
+      PORTD ^= (1<<hvilkenLED);
+      _delay_ms(FORSINKELSE);
+    }
+    PORTD = 0;
+    _delay_ms(FORSINKELSE);
+  }
+  return 0;
 }
 
