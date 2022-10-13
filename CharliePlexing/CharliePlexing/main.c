27325805fff8b34ecfab2c07946cb46b23ae5b5d
DNIIBOY <daniel@nettelfield.com>
2022-10-13 10:31:18 +0200

    add new CharliePlexing project

---
+++ CharliePlexing/CharliePlexing/main.c
@@ -0,0 +1,18 @@
+/*
+ * CharliePlexing.c
+ *
+ * Created: 13-10-2022 09:33:43
+ * Author : danie
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
