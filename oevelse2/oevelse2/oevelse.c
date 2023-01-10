181c1ea9d3958e7112e9da056f569c51919c6348
DNIIBOY <daniel@nettelfield.com>
2023-01-10 10:57:57 +0100

    complete oevelse2

--- oevelse2/oevelse2/oevelse.c
+++ oevelse2/oevelse2/oevelse.c
@@ -14,42 +14,48 @@
 
 int main()
 {
-	unsigned char i;
+  unsigned char i;
+  unsigned int currentLED;
 
-	// Klargøring af SWITCH-porten
-	initSwitchPort();
-	// Klargøring af LED-porten
-	initLEDport();
-	
-	// Test af switchStatus():
-	// Resultatet skrives til hele lysdiodeporten
-	// Testen afsluttes, når man samtidigt
-	// trykket på SW0 og SW1
-	i = 0;
-	while (i != 0b00000011)
-	{
-		i = switchStatus();
-		writeAllLEDs(i);
-	}
-	
-	// Tænd alle lysdioder i 2 sekunder
-	// og sluk så alle lysdioder
-	// Så kan vi se, at næste test starter
-	writeAllLEDs(0xFF);
-	_delay_ms(2000);
-	writeAllLEDs(0);
-	
-	// Test af switchOn():
-	// Læs på skift de 2 kontakter
-	// Trykkes knappen på PINB0, skal LED0 tænde
-	// For hver gang samme knap påvirkes, skal den tændte diode
-	// rykke 1 mod venstre. Trykkes knappen på PINB1, skal den
-	// tændte diode rykke 1 mod højre.
-	while (1)
-	{
-		for (i=0; i<=1; i++)
-		{
-		}
-	}
-	return 0;
+  // Klargøring af SWITCH-porten
+  initSwitchPort();
+  // Klargøring af LED-porten
+  initLEDport();
+
+  // Test af switchStatus():
+  // Resultatet skrives til hele lysdiodeporten
+  // Testen afsluttes, når man samtidigt
+  // trykket på SW0 og SW1
+  i = 0;
+  while (i != 0b00000011) {
+    i = switchStatus();
+    writeAllLEDs(i);
+  }
+
+  // Tænd alle lysdioder i 2 sekunder
+  // og sluk så alle lysdioder
+  // Så kan vi se, at næste test starter
+  writeAllLEDs(0xFF);
+  _delay_ms(2000);
+  writeAllLEDs(0);
+
+  // Test af switchOn():
+  // Læs på skift de 2 kontakter
+  // Trykkes knappen på PINB0, skal LED0 tænde
+  // For hver gang samme knap påvirkes, skal den tændte diode
+  // rykke 1 mod venstre. Trykkes knappen på PINB1, skal den
+  // tændte diode rykke 1 mod højre.
+  currentLED = 0;
+  while (1) {
+    currentLED %= 8;
+    if (switchOn(0)){
+      currentLED++;
+    }
+    else if (switchOn(1)){
+      currentLED--;
+    }
+    writeAllLEDs(1 << currentLED);
+    _delay_ms(150);
+  }
+  return 0;
 }
